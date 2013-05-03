fad5ca0cc35e9ca92ded57c5d0dba2506e40d295
hahastudio <guyuhao088@gmail.com>
2013-05-03 15:24:59 +0800

    Improved: no u in front of string

--- BlackJack.py
+++ BlackJack.py
@@ -2,15 +2,13 @@
 import random
 
 def init_cards(cards):
-	for color in u"♠♥♣♦":
-		for rank in (u"A", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9", u"10", u"J", u"Q", u"K"):
+	for color in ("♠", "♥", "♣", "♦"):
+		for rank in ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"):
 			cards.append(color + rank)
 	random.shuffle(cards)
-
-
 
 if __name__ == '__main__':
 	cards = []
 	init_cards(cards)
 	for card in cards:
-		print card.encode("utf-8"),
+		print card,
