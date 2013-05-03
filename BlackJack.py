0852b8aff603d373bceb5a0b4adc38342d7caed0
hahastudio <guyuhao088@gmail.com>
2013-05-03 15:17:18 +0800

    Fixed: no 10 in rank

--- BlackJack.py
+++ BlackJack.py
@@ -3,9 +3,11 @@ import random
 
 def init_cards(cards):
 	for color in u"♠♥♣♦":
-		for rank in u"A23456789JQK":
+		for rank in (u"A", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9", u"10", u"J", u"Q", u"K"):
 			cards.append(color + rank)
 	random.shuffle(cards)
+
+
 
 if __name__ == '__main__':
 	cards = []
