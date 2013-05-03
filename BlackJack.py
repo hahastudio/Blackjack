a79ce937e8048e4e68cbea1e18cf18bd92e06ad3
hahastudio <guyuhao088@gmail.com>
2013-05-03 20:25:34 +0800

    Add: Dealer's decision

--- BlackJack.py
+++ BlackJack.py
@@ -49,31 +49,96 @@ def ask_decision(player):
 	decision = decision.upper()
 	while decision not in ("H", "S"):
 		decision = raw_input("Invalid input. Please input \"H\" or \"S\" >")
+		decision = decision.upper()
 	return decision
+
+def ask_play_again():
+	decision = raw_input("Do you want to play again? (Y/N)>")
+	decision = decision.upper()
+	while decision not in ("Y", "N"):
+		decision = raw_input("Invalid input. Please input \"Y\" or \"N\" >")
+		decision = decision.upper()
+	return decision	
 
 def hit(cards, someone):
 	someone.append(cards.pop())
 
-if __name__ == '__main__':
-	cards = []
-	player = []
-	dealer = []
-	init_cards(cards)
-	init_deal(cards, dealer, player)
-	print "Player's(Your) card totals: %d" % sum_cards(player)
+def dealer_decision(cards, dealer):
+	while sum_cards(dealer) < 17:
+		hit(cards, dealer)
+		print "Dealer(PC) got cards: ",
+		show_cards(dealer)
+		print "Dealer's(PC) card totals: %d" % sum_cards(dealer)
+
+def player_win():
+	print "Player(You) wins!!!"
+
+def dealer_win():
+	print "Dealer(PC) wins."
+
+def drawn():
+	print "Drawn."
+
+def win(dealer, player):
 	if is_blackjack(player):
-		print "And Player(You) got a BlackJack!!!"
+		if is_blackjack(dealer):
+			drawn()
+		else:
+			player_win()
 	else:
-		while sum_cards(player) < 21:
-			if ask_decision(player) == "H":
-				hit(cards, player)
-				print "Player(You) got cards: ",
-				show_cards(player)
-				sum_player = sum_cards(player)
-				print "Player's(Your) card totals: %d" % sum_player
-				if sum_player > 21:
-					print "Busted!"
-					break
+		if is_blackjack(dealer):
+			dealer_win()
+		else:
+			sum_player = sum_cards(player)
+			sum_dealer = sum_cards(dealer)
+			if sum_player > 21:
+				if sum_dealer > 21:
+					drawn()
+				else:
+					dealer_win()
 			else:
-				break
-	
+				if sum_dealer > 21:
+					player_win()
+				else:
+					if sum_player > sum_dealer:
+						player_win()
+					elif sum_player == sum_dealer:
+						drawn()
+					else:
+						dealer_win()
+
+if __name__ == '__main__':
+	while True:
+		cards = []
+		player = []
+		dealer = []
+		init_cards(cards)
+		init_deal(cards, dealer, player)
+		print "Player's(Your) card totals: %d" % sum_cards(player)
+		if is_blackjack(player):
+			print "And Player(You) got a BlackJack!!!"
+		else:
+			while sum_cards(player) < 21:
+				if ask_decision(player) == "H":
+					hit(cards, player)
+					print "Player(You) got cards: ",
+					show_cards(player)
+					sum_player = sum_cards(player)
+					print "Player's(Your) card totals: %d" % sum_player
+					if sum_player > 21:
+						print "Busted!"
+						break
+				else:
+					break
+		print "It's Dealer(PC)'s turn."
+		print "Dealer(PC) got cards: ",
+		show_cards(dealer)
+		print "Dealer's(PC) card totals: %d" % sum_cards(dealer)
+		if is_blackjack(dealer):
+			print "And Dealer(PC) got a BlackJack!!!"
+		dealer_decision(cards, dealer)
+		if sum_cards(dealer) > 21:
+			print "Busted!"
+		win(dealer, player)
+		if ask_play_again() == "N":
+			break
